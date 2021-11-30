f48363864df1d11d2807b984eaecdc05256ad5f2
Francesco Nisi <francesconisi95@gmail.com>
2021-11-30 14:25:18 +0100

    project base

---
+++ src/script.js
@@ -0,0 +1,60 @@
+import "./style.css";
+import * as THREE from "three";
+import {
+  MapControls,
+  OrbitControls,
+} from "three/examples/jsm/controls/OrbitControls";
+import dat from "dat.gui";
+
+/*Scene*/
+const scene = new THREE.Scene();
+const canvas = document.querySelector("canvas.webgl");
+/*Meshe*/
+/*Cube*/
+const cube = new THREE.Mesh(
+  new THREE.BoxBufferGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "white" })
+);
+scene.add(cube);
+
+/*Sizes*/
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+/*Camera*/
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 5;
+scene.add(camera);
+
+/*Resize*/
+window.addEventListener("resize", () => {
+  /*Sizes Resize*/
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  /*Camera resize*/
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix;
+  /*Renderer*/
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/*Renderer*/
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.render(scene, camera);
+/*Controls*/
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+/*Animation*/
+const clock = new THREE.Clock();
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  controls.update;
+  window.requestAnimationFrame(tick);
+  renderer.render(scene, camera);
+};
+tick();
